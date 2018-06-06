4fc6185fd5d034d2339f2dd2f9b6acab211bfaee
Andy Allison <andy@talkbe.com>
2018-06-06 09:11:20 +0100

    Refactor components

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,19 +1,15 @@
 import React from 'react';
 
-const Option = (props) => {
-  console.log('testign');
-
-  return (
-    <li>
-      {props.text}
-      <button onClick={(e) => {
-        props.handleDeleteOption(props.text)
-      }}
-      >
-        Remove
-      </button>
-    </li>
-  );
-};
+const Option = (props) => (
+  <li>
+    {props.text}
+    <button onClick={(e) => {
+      props.handleDeleteOption(props.text)
+    }}
+    >
+      Remove
+    </button>
+  </li>
+);
 
 export default Option;
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import Option from './Option';
 
-const Options = (props) => {
-  return (
-    <div>
-      {props.options.length === 0 && <p>Pease enter an option to get started!</p>}
-      {props.options.length > 0 && <p>You Have {props.options.length}</p>}
-      <ul>
-        {
-          props.options.map(option => (
-            <Option
-              key={option}
-              text={option}
-              handleDeleteOption={props.handleDeleteOption}
-            />
-          ))
-        }
-        {props.options.length > 0 && <button onClick={props.handleDeleteOptions}>Remove All</button>}
-      </ul>
-    </div>
-  );
-};
+const Options = (props) => (
+  <div>
+    {props.options.length === 0 && <p>Pease enter an option to get started!</p>}
+    {props.options.length > 0 && <p>You Have {props.options.length}</p>}
+    <ul>
+      {
+        props.options.map(option => (
+          <Option
+            key={option}
+            text={option}
+            handleDeleteOption={props.handleDeleteOption}
+          />
+        ))
+      }
+      {props.options.length > 0 && <button onClick={props.handleDeleteOptions}>Remove All</button>}
+    </ul>
+  </div>
+);
 
 export default Options;
